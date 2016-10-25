b50b11a6c010cbec329d9c97c00b04d9e8cb3f93
chptiger <xx65@njit.edu>
2016-10-25 13:13:16 -0700

    define many to one relationship in note

---
+++ app/Note.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'note','user_id'
+    ];
+
+    public function user() {
+        return $this->belongsToMany('App\User');
+    }
+}
