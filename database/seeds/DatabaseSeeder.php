ea27ebdf085fc1353c9c823477e6ca6221f62237
chptiger <xx65@njit.edu>
2016-10-25 12:42:15 -0700

    insert data into table users and notes command: php artisan db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'password' => Hash::make('12345678'),
+            'email' => '456@gmail.com',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
+        DB::table('users')->insert([
+            'password' => Hash::make('12345678'),
+            'email' => '789@gmail.com',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        // insert note info into notes
+        DB::table('notes')->insert([
+            'title' => 'note1',
+            'note' => 'good morning',
+            'user_id' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        DB::table('notes')->insert([
+            'title' => 'note2',
+            'note' => 'good afternoon',
+            'user_id' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        DB::table('notes')->insert([
+            'title' => 'note3',
+            'note' => 'good evening',
+            'user_id' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        // insert note info into notes
+        DB::table('notes')->insert([
+            'title' => 'note1',
+            'note' => 'good morning',
+            'user_id' => '2',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        DB::table('notes')->insert([
+            'title' => 'note2',
+            'note' => 'good afternoon',
+            'user_id' => '2',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        DB::table('notes')->insert([
+            'title' => 'note3',
+            'note' => 'good evening',
+            'user_id' => '2',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
 
